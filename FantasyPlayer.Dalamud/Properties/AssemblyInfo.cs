a17a855e195d9de45a2ba80479edc540540ca2c3
Biccy <contact@bix.moe>
2020-12-05 11:38:03 +1100

    Bump version

--- FantasyPlayer.Dalamud/Properties/AssemblyInfo.cs
+++ FantasyPlayer.Dalamud/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 #if DEBUG
-[assembly: AssemblyVersion("1.2.2.*")]
-[assembly: AssemblyFileVersion("1.2.2.*")]
+[assembly: AssemblyVersion("1.2.3.*")]
+[assembly: AssemblyFileVersion("1.2.3.*")]
 #else
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
 #endif
