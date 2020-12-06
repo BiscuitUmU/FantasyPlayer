d28a345539dba55cdd5e7941b140c4b6d75a9285
Biccy <contact@bix.moe>
2020-12-07 10:03:06 +1100

    FP 1.2.4 - SP 1.1.3

--- FantasyPlayer.Dalamud/Properties/AssemblyInfo.cs
+++ FantasyPlayer.Dalamud/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 #if DEBUG
-[assembly: AssemblyVersion("1.2.3.*")]
-[assembly: AssemblyFileVersion("1.2.3.*")]
+[assembly: AssemblyVersion("1.2.4.*")]
+[assembly: AssemblyFileVersion("1.2.4.*")]
 #else
-[assembly: AssemblyVersion("1.2.3.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyVersion("1.2.4.0")]
+[assembly: AssemblyFileVersion("1.2.4.0")]
 #endif
